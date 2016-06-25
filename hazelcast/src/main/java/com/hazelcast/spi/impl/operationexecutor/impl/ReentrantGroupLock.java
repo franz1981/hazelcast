6013548a7bc6717f9aa4b88c4d575c317602fabc
Francesco Nigro <nigro.fra@gmail.com>
2016-06-25 10:16:50 +0200

    Formatted sources. Added ReentrantGroupLock to manage lock of groups

---
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationexecutor/impl/ReentrantGroupLock.java
@@ -0,0 +1,59 @@
+package com.hazelcast.spi.impl.operationexecutor.impl;
+
+final class ReentrantGroupLock {
+
+   private final SparsePaddedAtomicIntegerArray groupLocks;
+   //TODO preferred byte array counter to reduce memory footprint
+   private final ThreadLocal<byte[]> groupAccesses;
+
+   public ReentrantGroupLock(final int groups) {
+      this.groupAccesses = new ThreadLocal<byte[]>() {
+         @Override
+         protected byte[] initialValue() {
+            return new byte[groups];
+         }
+      };
+      this.groupLocks = new SparsePaddedAtomicIntegerArray(groups);
+   }
+
+   public boolean tryLock(final int index) {
+      final byte[] threadGroupAccesses = this.groupAccesses.get();
+      final int groupAccessCount = threadGroupAccesses[index] & 255;
+      if (groupAccessCount > 0) {
+         final int nextGroupAccessCount = groupAccessCount + 1;
+         if (nextGroupAccessCount > 255) {
+            throw new IllegalStateException("reached the maximum reentrant count!");
+         }
+         threadGroupAccesses[index] = (byte) nextGroupAccessCount;
+         return true;
+      }
+      else {
+         //TODO MEASURE!! try to perform a wait-free (if supported) atomic increment -> could scale better
+         //it succeed only if is the first different thread that try to enter
+         if (groupLocks.getAndIncrement(index) == 0) {
+            threadGroupAccesses[index] = 1;
+            return true;
+         }
+         else {
+            return false;
+         }
+      }
+   }
+
+   public void unlock(final int index) {
+      final byte[] threadGroupAccesses = this.groupAccesses.get();
+      final int groupAccessCount = threadGroupAccesses[index] & 255;
+      if (groupAccessCount > 0) {
+         final int nextGroupAccessCount = groupAccessCount - 1;
+         threadGroupAccesses[index] = (byte) nextGroupAccessCount;
+         //release only if is the last access to be removed
+         if (nextGroupAccessCount == 0) {
+            this.groupLocks.set(index, 0);
+         }
+      }
+      else {
+         throw new IllegalStateException("can't unlock without a successfull tryLock first!");
+      }
+   }
+
+}
