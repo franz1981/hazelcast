da697c18b1ba745096aa31e7b8d7c05aac096aad
Francesco Nigro <nigro.fra@gmail.com>
2016-06-25 10:32:55 +0200

    Added utilities methods

--- hazelcast/src/main/java/com/hazelcast/spi/impl/operationexecutor/impl/ReentrantGroupLock.java
+++ hazelcast/src/main/java/com/hazelcast/spi/impl/operationexecutor/impl/ReentrantGroupLock.java
@@ -3,7 +3,7 @@ package com.hazelcast.spi.impl.operationexecutor.impl;
 final class ReentrantGroupLock {
 
    private final SparsePaddedAtomicIntegerArray groupLocks;
-   //TODO preferred byte array counter to reduce memory footprint
+   //TODO preferred byte array counter to reduce memory footprint -> BUT use unsigned bytes
    private final ThreadLocal<byte[]> groupAccesses;
 
    public ReentrantGroupLock(final int groups) {
@@ -16,7 +16,17 @@ final class ReentrantGroupLock {
       this.groupLocks = new SparsePaddedAtomicIntegerArray(groups);
    }
 
-   public boolean tryLock(final int index) {
+   public final int groupAccesses(final int index) {
+      final byte[] threadGroupAccesses = this.groupAccesses.get();
+      final int groupAccessCount = threadGroupAccesses[index] & 255;
+      return groupAccessCount;
+   }
+
+   public final int groups() {
+      return this.groupLocks.length();
+   }
+
+   public final boolean tryLock(final int index) {
       final byte[] threadGroupAccesses = this.groupAccesses.get();
       final int groupAccessCount = threadGroupAccesses[index] & 255;
       if (groupAccessCount > 0) {
@@ -28,7 +38,7 @@ final class ReentrantGroupLock {
          return true;
       }
       else {
-         //TODO MEASURE!! try to perform a wait-free (if supported) atomic increment -> could scale better
+         //TODO MEASURE!! try to perform a wait-free (if supported) atomic increment -> scale better when contended?
          //it succeed only if is the first different thread that try to enter
          if (groupLocks.getAndIncrement(index) == 0) {
             threadGroupAccesses[index] = 1;
@@ -40,7 +50,7 @@ final class ReentrantGroupLock {
       }
    }
 
-   public void unlock(final int index) {
+   public final void unlock(final int index) {
       final byte[] threadGroupAccesses = this.groupAccesses.get();
       final int groupAccessCount = threadGroupAccesses[index] & 255;
       if (groupAccessCount > 0) {
@@ -48,6 +58,7 @@ final class ReentrantGroupLock {
          threadGroupAccesses[index] = (byte) nextGroupAccessCount;
          //release only if is the last access to be removed
          if (nextGroupAccessCount == 0) {
+            //ATTENTION!! do not use a plain/ordered store -> by lock's contract loads can't escape the mutual esclusion region!
             this.groupLocks.set(index, 0);
          }
       }
